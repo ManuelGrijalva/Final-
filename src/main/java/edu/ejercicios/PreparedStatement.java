e42ec5193606b16478ea985d8d0c993e2379aa4e
Manuel Ernesto Grijalva Tenas <125087957+ManuelGrijalva@users.noreply.github.com>
2023-10-30 17:52:09 -0600

    Add files via upload

---
+++ src/main/java/edu/ejercicios/PreparedStatement.java
@@ -0,0 +1,16 @@
+package edu.ejercicios;
+
+import java.sql.ResultSet;
+
+public class PreparedStatement {
+    public void close() {
+    }
+
+    public void setString(int i, String username) {
+    }
+
+    public ResultSet executeQuery() {
+        return null;
+    }
+
+}
